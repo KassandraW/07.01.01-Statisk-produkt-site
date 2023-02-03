79df0682db53760c75c7b6a0214a25b67809a8d8
KassandraW <119849798+KassandraW@users.noreply.github.com>
2023-02-03 13:32:55 +0100

    fået template til at virke wahoo

--- js/productlist.js
+++ js/productlist.js
@@ -1,4 +1,4 @@
-fetch("https://kea-alt-del.dk/t7/api/products")
+fetch("https://kea-alt-del.dk/t7/api/products?limit=100")
   .then((res) => res.json())
   .then((data) => showProducts(data));
 
@@ -9,9 +9,14 @@ function showProducts(products) {
 
 function showProduct(product) {
   //fange template
+
+  const template = document.querySelector("#smallProductTemplate").content;
   //lave en kopi
+  const copy = template.cloneNode(true);
   //ændre indhold
+
   //appende hvor den skal være i DOM
+  document.querySelector(".grid-produkter").appendChild(copy);
 }
 
 // SE https://www.youtube.com/watch?v=VAAjHOw7x_Q
