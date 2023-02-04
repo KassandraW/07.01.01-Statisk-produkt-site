b69cbcac3f613f623f929553c272bef9f1f0b58a
KassandraW <kassandrawadum@hotmail.com>
2023-02-04 14:22:08 +0100

    produktliste ligner noget nu

--- js/productlist.js
+++ js/productlist.js
@@ -9,14 +9,32 @@ function showProducts(products) {
 
 function showProduct(product) {
   //fange template
-
   const template = document.querySelector("#smallProductTemplate").content;
+
   //lave en kopi
   const copy = template.cloneNode(true);
+
   //ændre indhold
+  copy.querySelector(".title").textContent = product.productdisplayname;
+  copy.querySelector(
+    ".info"
+  ).textContent = `${product.category} | ${product.brandname}`;
+  copy.querySelector(".price").textContent = `${product.price} kr,-`;
 
   //appende hvor den skal være i DOM
   document.querySelector(".grid-produkter").appendChild(copy);
 }
 
 // SE https://www.youtube.com/watch?v=VAAjHOw7x_Q
+
+{
+  /* <template id="smallProductTemplate">
+              <a class="card-container" href="product.html">
+                <div class="prod-image"></div>
+                <div class="prod-txt-container">
+                  <div class="title">Produktnavn</div>
+                  <div class="price">kr. xxx</div>
+                </div>
+              </a>
+            </template> */
+}
