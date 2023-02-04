dc2cf0c6a3b42752e34e1fe96a6cafb793d8b0bf
KassandraW <kassandrawadum@hotmail.com>
2023-02-04 13:58:01 +0100

    dynamisk product

--- js/product.js
+++ js/product.js
@@ -4,7 +4,12 @@ fetch("https://kea-alt-del.dk/t7/api/products/1165")
 
 function showProduct(product) {
   console.log(product);
-}
 
+  document.querySelector(".prod-name").textContent = product.productdisplayname;
+  document.querySelector(".prod-brand").textContent = product.brandname;
+  document.querySelector(".prod-price").textContent = `${product.price} kr,-`;
+  document.querySelector(".prod-color").textContent = product.basecolour;
+  document.querySelector("img").src = product.brandimage;
+}
 
 // SE DEN HER VIDEO https://www.youtube.com/watch?v=pmWaikKHV0s
