41a81cad9dfc164205c293ef123bfd3c7475540e
KassandraW <kassandrawadum@hotmail.com>
2023-02-04 14:32:41 +0100

    billede på produktside

--- js/product.js
+++ js/product.js
@@ -9,7 +9,7 @@ function showProduct(product) {
   document.querySelector(".prod-brand").textContent = product.brandname;
   document.querySelector(".prod-price").textContent = `${product.price} kr,-`;
   document.querySelector(".prod-color").textContent = product.basecolour;
-  document.querySelector("img").src = product.brandimage;
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
 }
 
 // SE DEN HER VIDEO https://www.youtube.com/watch?v=pmWaikKHV0s
